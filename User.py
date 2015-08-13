844586542463a66f7e9057fd3ffd96313e9d749e
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2015-08-13 12:42:49 -0700

    Catch decode error

--- User.py
+++ User.py
@@ -80,7 +80,7 @@ class User:
             status = json.loads(response.text, encoding='utf-8')["presence"]
 
             return status == "active"
-        except (requests.exceptions.ConnectionError,NameError,KeyError):
+        except (requests.exceptions.ConnectionError,NameError,KeyError,ValueError):
             print "Error fetching online status for " + self.get_user_handle()
             return False
 
